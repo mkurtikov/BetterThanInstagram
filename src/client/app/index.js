a21506e85ebef18cb286b88f740e4947b09ead37
Ksenia <6terranova9@gmail.com>
2018-03-21 11:00:49 +0200

    urgent commit

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Main from './components/Navigation';
+import Navigation from './components/Navigation';
 
 render((
-    <Main />
+    <Navigation />
 ), document.getElementById('app'));
 
 
